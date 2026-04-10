b47bdd02ed59993f4052fed54cc2e8a25eac0528
Vladyslav Dotsenko <vladik.28.06.2006@gmail.com>
2026-04-10 13:34:04 +0200

    -feat generate_project:base_path.mkdir

--- crtproject/generator.py
+++ crtproject/generator.py
@@ -5,14 +5,15 @@ TEMPLATES_DIR = Path(__file__).parent / 'templates'
 
 
 def generate_project(template: str, name: str, base_path: Path):
+    base_path.mkdir(parents=True, exist_ok=True)
 
     template_path = TEMPLATES_DIR / template
     project_path = base_path / name
 
-    if not template_path.is_dir():
+    if not template_path.exists():
         raise FileNotFoundError(f'Template {template} does not exist')
     if project_path.exists():
-        raise FileExistsError(f'Project {project_path} already existы')
+        raise FileExistsError(f'Project {project_path} already exists')
 
     shutil.copytree(template_path, project_path)
 
